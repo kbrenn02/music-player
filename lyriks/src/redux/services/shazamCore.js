888cdfa6ee578e7b05f5fdd034aadf70ff7854a9
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-07-16 21:25:28 -0700

    connected to api and data is flowing

--- lyriks/src/redux/services/shazamCore.js
+++ lyriks/src/redux/services/shazamCore.js
@@ -6,15 +6,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1/charts/world?country_code=DZ',
-        prepareHeaders: () => {
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
             headers.set('x-rapidapi-key', 'c2e5faeed9msh1ed7261b6244abbp1fe90ejsnd83eff727bdf');
 
             return headers;
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: '/charts/world' })
+        getTopCharts: builder.query({ query: () => '/charts/world?country_code=DZ' })
     }),
 });
 
