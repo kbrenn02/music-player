3abd28ea2078133b360d935506acfa491fbc0e74
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-07-16 21:18:14 -0700

    adjusted api call because I bought the one used in the tutorial

--- lyriks/src/redux/services/shazamCore.js
+++ lyriks/src/redux/services/shazamCore.js
@@ -2,19 +2,22 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const options = {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-key': 'c2e5faeed9msh1ed7261b6244abbp1fe90ejsnd83eff727bdf',
-      'x-rapidapi-host': 'shazam.p.rapidapi.com'
-    }
-};
-  
-fetch('https://shazam.p.rapidapi.com/charts/list', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
 
-export const shazamApi = createApi({
+export const shazamCoreApi = createApi({
     reducerPath: 'shazamApi',
-})
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1/charts/world?country_code=DZ',
+        prepareHeaders: () => {
+            headers.set('x-rapidapi-key', 'c2e5faeed9msh1ed7261b6244abbp1fe90ejsnd83eff727bdf');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: '/charts/world' })
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
