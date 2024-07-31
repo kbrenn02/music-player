9a4df4e78751cd0efb024d16e6205936dd99099c
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-07-30 17:46:14 -0700

    Completed artistCard and it actually works, API calls and all

--- lyriks/src/components/ArtistCard.jsx
+++ lyriks/src/components/ArtistCard.jsx
@@ -1,5 +1,24 @@
-const ArtistCard = () => {
-  <div>ArtistCard</div>;
+import { useNavigate } from "react-router-dom";
+
+
+const ArtistCard = ({ track }) => {
+
+    const navigate = useNavigate();
+
+    // console.log("track: ", track?.attributes?.artwork?.url)
+
+    return (
+        <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 
+        backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+            onClick={() => navigate(`/artists/${track?.relationships?.artists?.data[0].id}`)}>
+            <img 
+                src={track?.attributes?.artwork?.url} 
+                alt="artist" 
+                className="w-full h-56 rounded-lg"
+            />
+            <p className="mt-4 font-semibold text-lg text-white truncate">{track?.attributes?.artistName}</p>
+        </div>
+    )
 };
 
 export default ArtistCard;
