9a91127809110cb639b7ab67a7047edca747b12b
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-07-30 17:31:00 -0700

    Outline of the Around you component. Songs actually appear without error

--- lyriks/src/pages/AroundYou.jsx
+++ lyriks/src/pages/AroundYou.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useSelector } from "react-redux";
 
 import { Error, Loader, SongCard } from '../components'
-
+import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
 
 
 const AroundYou = () => {
@@ -11,6 +11,7 @@ const AroundYou = () => {
     const [country, setCountry] = useState('');
     const [loading, setLoading] = useState(true);
     const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
     console.log(country)
 
@@ -22,9 +23,26 @@ const AroundYou = () => {
             .finally(() => setLoading(false))
     }, [country]);
 
-    return (
-        <div>
+    if(isFetching && loading) return <Loader title="Loading songs around you" />
 
+    if(error && country) return <Error />
+
+    return (
+        <div className="flex flex-col">
+            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around You</h2>
+
+            <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+                {data?.map((song, i) => i < 50 && (
+                    <SongCard 
+                        key={i}
+                        song={song}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        data={data}
+                        i={i}
+                    />
+                ))}
+            </div>
         </div>
     )
 
