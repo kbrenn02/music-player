29d27e5487d94296cbd7280bac2eb0fe486ff2ca
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-07-30 17:35:03 -0700

    Complete top charts component

--- lyriks/src/pages/AroundYou.jsx
+++ lyriks/src/pages/AroundYou.jsx
@@ -29,7 +29,9 @@ const AroundYou = () => {
 
     return (
         <div className="flex flex-col">
-            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around You</h2>
+            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+                Around You <span className="font-black">{country}</span>
+            </h2>
 
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {data?.map((song, i) => i < 50 && (
