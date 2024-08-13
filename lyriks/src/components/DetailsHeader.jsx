48afd853d2f2888e8368aaf1e522bc821d4cf7a8
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-08-12 23:28:53 -0700

    in theory, all the data should be showing (and right now there are no errors), but the data still isn't showing

--- lyriks/src/components/DetailsHeader.jsx
+++ lyriks/src/components/DetailsHeader.jsx
@@ -6,7 +6,11 @@ import { Link } from 'react-router-dom'
 const DetailsHeader = ({ artistId, artistData, songData }) => {
 
     console.log('artistid in detailsheader', artistId)
+    // console.log('songid in detailsheader', songId)
     console.log('detailsheader song data', songData)
+    const songId = Object.keys(songData?.resources['shazam-songs'])[0]; // Get the first artist ID
+    const songAttributes = songData?.resources['shazam-songs'][songId]?.attributes;
+    console.log('attributes', songAttributes)
    
 
     return (
@@ -16,29 +20,29 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
             <div className='absolute inset-0 flex items-center'>
                 <img 
                     alt='art'
-                    // Fix this: show the artist image or the song cover art
+                    // Fix this: show the artist image or the song cover art. No errors but not showing artist avatar
                     src={artistId ? artistData?.attributes?.artwork?.url
                         .replace('{w}', '500')
                         .replace('{h}', '500') 
-                        : songData?.attributes?.artwork?.url}
+                        : songAttributes?.images?.artistAvatar}
                     className='sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black'
                 />
 
-                <div className="ml-5">
-                    {/* Fix this: should be showing the artist name. If it doesn't, it should show the song title */}
-                    <p className='font-bold sm:text-3xl text-xl text-white'>{artistId ? artist?.name : songData?.title}</p>
+                <div className="ml-5 w-full h-full">
+                    {/* Fix this: should be showing the artist name. If it doesn't, it should show the song title. Messed with sizing here and it still doesnt show. */}
+                    <p className='font-bold h-full sm:text-3xl text-xl text-white'>{artistId ? artistData?.name : songAttributes?.artist}</p>
                     {!artistId && (
-                        <Link to={`/artists/${songData?.relationships?.artists?.data[0].id}`}>
+                        <Link to={`/artists/${artistId}`}>
                             {/* Fix this: This should show the artist name */}
-                            <p className='text-base text-gray-400 mt-2'>{songData?.attributes?.artistName}</p>
+                            <p className='text-base text-gray-400 mt-2'>{songAttributes?.artist}</p>
                         </Link>
                     )}
 
                     <p className='text-base text-gray-400 mt-2'>
                         {/* Fix this: This should show the artist genre or the song genre */}
                         {artistId
-                          ? artist?.genreNames[0]
-                          : songData?.genres?.primary}
+                          ? artistData?.genreNames[0]
+                          : songAttributes?.genres?.primary}
                     </p>
                 </div>
             </div>
